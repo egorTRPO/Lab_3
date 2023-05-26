f2447088c9d50ffc4b3fd693536bf2806974dfb0
egorTRPO <ezidovic@gmail.com>
2023-05-26 17:26:56 +0300

    Changed function MyBottomAppBar and completed project

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
@@ -1,6 +1,7 @@
 package com.raywenderlich.android.lab1.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -74,5 +75,8 @@ fun MyTopAppBar(scaffoldState: ScaffoldState, scope: CoroutineScope){
 
 @Composable
 fun MyBottomAppBar(){
-
+    BottomAppBar(
+        content = {},
+        backgroundColor = colorResource(id = R.color.colorPrimary)
+    )
 }
